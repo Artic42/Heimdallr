b58dde61f13caa431d5557f2ad764c17a78adbc8
Artic42 <main@artic42.com>
2020-08-29 09:09:01 +0200

    First commit

--- Source/Header/GPIO.h
+++ Source/Header/GPIO.h
@@ -1,44 +1,52 @@
 /*
-File Name:	<++>
+File Name:	GPIO.h
 //
-Compiler:	<++>
-Author:		<++>
-Description:	<++>
-Testd on:	<++>
+Compiler:	gcc
+Author:		Artic42
+Description:	Header file for the GPIO.c, contains the necessary offset for the GPIO of Raspberry Pi Zero and Raspberry Pi 4. Important the initial adress of the different Pi models aren't the same, during compilation target must be selected.
+Testd on:	Raspberry Pi 4 B // 2Gb RAM
 */
 
 /****************************************
 *	Include				*
 *****************************************/
 
-<++>
+#include "io.h" //This is the file that defines the constant that are diferent depending on the model of Raspberry Pi 4
 
 /****************************************
 *	Public Function Prtotype	*
 ****************************************/
 
-<++>
+void gpioModeSelect (int gpioNumber, int Mode);
 
-/****************************************
-*	Public Definitions		*
-****************************************/
+void gpioSet (int gpioNumber);
+void gpioClear (int gpioNumber);
+void gpioWriteValue (int gpioNumber, char value);
 
-<++>
+char gpioRead (int gpioNumber);
 
-/****************************************
-*	Public Global Variables		*
-****************************************/
-
-<++>
 
 /****************************************
 *	Public Constants		*
 ****************************************/
 
-<++>
+#define GPIO0 0
+#define GPIO1 1
+//Completar con todos los GPIO más adelante
+
+#define BTRUE 1
+#define BFALSE 0
+
+#define INPUT_MODE 0
+#define OUTPUT_MODE 1
+#define FUNC_0 4
+#define FUNC_1 5
+#define FUNC_2 6
+#define FUNC_3 7
+#define FUNC_4 3
+#define FUNC_5 2
+
 
 /****************************************
-*	Structures			*
+*	Adress offset		*
 ****************************************/
-
-<++>
