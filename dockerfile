e4cc9f63607ae0f3479c833f7f72b18515288e1c
Artic42 <main@artic42.com>
2023-10-06 20:45:45 +0200

    More tries Configure web server to accept forms #6

--- dockerfile
+++ dockerfile
@@ -7,13 +7,18 @@ RUN rm -rf /usr/share/nginx/html/*
 RUN apk add php
 RUN apk add php-fpm
 RUN apk add php-json
+RUN apk add php-mysqli
+RUN apk add php-session
+RUN mkdir -p /run/php
 
 
 # Install python3 and pip3 and necessary packages
 RUN apk add py3-pip
 
 # Copy files from repository to container
-COPY web /usr/share/nginx/html
+COPY web /usr/share/nginx
+COPY nginx.conf /etc/nginx/nginx.conf
 
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
+
--- web/php/test.php
+++ web/php/test.php
@@ -1,4 +1,15 @@
-$myFile = fopen("test.txt", "w") or die("Unable to open file!");
-$text = $_POST['name'];
-fwrite($myFile, $text);
-fclose($myFile);
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $data = "Name: $name\nEmail: $email\n";
+
+    // Specify the path to the file where you want to store the data
+    $file_path = "/var/www/html/form_data.txt";
+
+    // Open the file in append mode and write the data
+    file_put_contents($file_path, $data, FILE_APPEND | LOCK_EX);
+
+    echo "Data has been successfully saved.";
+}
+?>
